20ede72107e8e2e5b0f265f3692f6ef58d850133
anto.duxx99 <antoniodxx99@gmail.com>
2026-03-27 13:13:42 +0100

    created word and character count report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count, get_character_count
+from stats import get_word_count, get_character_count, char_dict
 
 def get_book_text(filepath):
     with open(filepath) as book:
@@ -7,7 +7,18 @@ def get_book_text(filepath):
 def main(filepath):
     text = get_book_text(filepath)
     word_count = get_word_count(text)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
-    get_character_count(text)
+    print("--------- Character Count -------")
+    character_count = get_character_count(text)
+    char_list = char_dict(character_count)
+    for item in char_list:
+        char = item['char']
+        count = item['num']
+        if char.isalpha():
+            print(f'{char}: {count}')
+    print("============= END ===============")
 
 main("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -4,14 +4,22 @@ def get_word_count(text):
     return len(words)
 
 def get_character_count(text):
-    character_count = {
-
-    }
+    character_count = {}
     for char in text:
         char = char.lower()
         if char in character_count:
             character_count[char] += 1
         else:
             character_count[char] = 1
-    print(character_count)
+    return character_count
 
+def char_dict(character_count):
+    char_list = []
+    for key, value in character_count.items():
+        char_list.append({"char": key, "num": value})
+    def sort_on(items):
+        return items["num"]
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+
+        
